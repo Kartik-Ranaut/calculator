c41ec035302a2971d28e610ffc04acb0e53dbd18
Kartik-Ranaut <ranautk6@gmail.com>
2023-12-21 18:07:30 +0530

    js added

---
+++ index.js
@@ -0,0 +1,11 @@
+function calc(value) {
+    document.getElementById("result").value += value;
+}
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+function calculate() {
+    var expression = document.getElementById("result").value;
+    var result = eval(expression);
+    document.getElementById("result").value = result;
+}
